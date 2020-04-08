c16f10cc8031f22d19f3f7a763df25c35890dcdc
Howard Ludewig <ludewigh@gmail.com>
2020-04-08 17:48:43 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,30 @@
 module "vpc" {
   source  = "app.terraform.io/Ludewigh/vpc/aws"
   version = "2.33.0"
+
+    name = "ludeiwgh-simple-example"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "euw1-az3"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_ipv6 = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
+
+  tags = {
+    Owner       = "ludewigh"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name-ludewigh"
+  }
 }
